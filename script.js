8e97152b84d5b2fe47e66917aea7c9003a634ea3
PedroMagno1996 <133236110+PedroMagno1996@users.noreply.github.com>
2023-08-10 17:45:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Utilize apenas imagens locais neste vetor, não links externos, não há suporte
 */
 
 var imagens = [
-    "imgs/FeedbackWifi.jpg",
+    "imgs/FeedbackWifi.png",
     "imgs/Keepers.png",
     "imgs/gifcanva2.gif",
     "imgs/CuidadoPrincipio.png",
