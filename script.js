56223bf83100ffc7ba8eefac85571a5bd2f28897
NiloCan <titolianchen3341@gmail.com>
2023-10-05 10:37:52 -0300

    add nova imagem de cervejas

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ Utilize apenas imagens locais neste vetor, não links externos, não há suporte
 */
 
 var imagens = [
+    "imgs/cervejaAtualizado.jpg",
     "imgs/FeedbackWifi.png",
     "imgs/Keepers.png",
     "imgs/gifcanva2.gif",
