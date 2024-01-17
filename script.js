3ee4d313d8f0916e670474bc84b0ece0dc607d8f
Geanny Thiesen <131676850+geannypthiesen@users.noreply.github.com>
2024-01-17 09:23:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var links = [
 
 ];
 
-var tempoImagens = 12000; // tempo em milissegundos para exibir cada imagem
+var tempoImagens = 18000; // tempo em milissegundos para exibir cada imagem
 var tempoLinks = 45000; // tempo em milissegundos para exibir cada link
 var indiceImagens = 0; // índice atual da imagem
 var indiceLinks = 0; // índice atual do link
