59534ad0d57ecd9e3e9d8deaa596b3157112d7b8
Geanny Thiesen <131676850+geannypthiesen@users.noreply.github.com>
2024-01-25 08:49:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var imagens = [
     "imgs/produtividade02.gif",
     "imgs/Keepers.png",
     "imgs/gifcanva2.gif",
+    "imgs/gif_produtividade-2-(gif-pt-1).gif",
+    "imgs/gif_produtividade-2-(gif-pt-2).gif",
     "imgs/agilistas.gif",
     "imgs/MentoriaConvite.png",
     "imgs/Limpeza.gif",
@@ -13,8 +15,7 @@ var imagens = [
     "imgs/JaneiroBranco4.png",
     "imgs/JaneiroBranco5.png",
     "imgs/JaneiroBranco6.png",
-    "imgs/gif_produtividade-2-(gif-pt-1).gif",
-    "imgs/gif_produtividade-2-(gif-pt-2).gif",
+    
     
 ]; // array com as imagens
 
