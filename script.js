25f12b0d8e94621fda6a8d6e30c42f66f989430b
Nilo Can <titolianchen3341@gmail.com>
2024-07-25 09:06:57 -0300

    removendo live entre chaves

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var imagensDpsDas18 = [
 
 ];
 var imagens = [
-    "imgs/EntreChavesLive.gif",
     "imgs/evolve.gif",
     "imgs/semana-4_produtividade1.gif",
     "imgs/semana-4_produtividade2.gif",
