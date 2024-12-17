45df66ce13fb28b63505e28c8942eccbcb837d9e
Nilo Can <titolianchen3341@gmail.com>
2024-12-17 14:45:23 -0300

    removendo sintase em casa

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const IMAGENS = [
 	'imgs/GifTV_Garden_EntreChaves.gif',
 	'imgs/GifTV_Agilistas.gif',
 	'imgs/[MKT]-RedesDti.gif',
-	'imgs/[MKT]-SintaSeEmCasa.gif',
 ];
 
 const IMAGENS_DPS_18 = [];
