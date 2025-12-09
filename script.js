011464032e9b96fa36dec9043258decca830a3cb
pedrocaetanodti <pedro.caetano@dtidigital.com.br>
2025-12-09 10:55:16 -0300

    add tv novos crafters 2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const IMAGENS = [
 	['imgs/GifTV_Garden_EntreChaves.gif', 15],
 	['imgs/GifTV_Agilistas.gif', 10],
+	['imgs/TvNovosCrafters2.png', 20],
 	['imgs/trabalhoHibrido.png', 20],
 	['imgs/trabalhoHibrido2.png', 20],
 	['imgs/VendaPcs.jpg', 25],
